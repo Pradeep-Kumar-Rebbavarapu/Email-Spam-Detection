c4326b0fd674182abc58dc8e4bf1c61ce132fd47
Pradeep_Kumar_Rebbavarapu <wardengaming1807@gmail.com>
2023-10-22 21:52:10 +0530

    init

---
+++ api/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+import threading
+
+class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api'
+   
+
+    
---
+++ api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,JsonResponse
+from django.conf import settings
+BASE_DIR = settings.BASE_DIR
+import pickle
+import os
+
+model = pickle.load(open(os.path.join(BASE_DIR,'model.pkl'),'rb'))
+
+def index(request):
+    if request.method=="POST":
+        text = request.POST.get('message')
+        message = [text]
+        print(message)
+        prediction = model.predict(message)
+        if prediction==1:
+            spam = "This is a spam message"
+        else:
+            spam = "This is not a spam message"
+        return render(request,'index.html',{'prediction_text':spam})
+    return render(request,'index.html')
+
+
+    
